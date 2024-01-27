a15e683e6ec9dcbdbac6b9f9f3227f8b10869841
mobina <mobinajafari.it@gmail.com>
2024-01-27 09:41:47 +0330

    first message

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log('starting function');
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      console.log('starting error');
+      alert('could not get your position');
+    }
+  );
+}
