a81cfa8e665eff295b67b774a586fab8bde896df
mobina <mobinajafari.it@gmail.com>
2024-01-28 10:39:07 +0330

    save work 2024.01.28

--- script.js
+++ script.js
@@ -18,7 +18,16 @@ if (navigator.geolocation) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
 
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      const coords = [latitude, longitude];
+
+      var map = L.map('map').setView(coords, 16);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords).addTo(map).bindPopup("You're here !").openPopup();
     },
     function () {
       console.log('starting error');
